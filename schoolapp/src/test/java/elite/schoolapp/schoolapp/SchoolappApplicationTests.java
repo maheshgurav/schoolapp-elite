bd2aaa16be57554466f1d8891b33c3158002cdea
Priyu <priydarshna@gmail.com>
2023-01-05 06:50:13 +0530

    Initial commit for controllers

---
+++ schoolapp/src/test/java/elite/schoolapp/schoolapp/SchoolappApplicationTests.java
@@ -0,0 +1,11 @@
+package elite.schoolapp.schoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolappApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
